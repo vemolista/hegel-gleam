41a2c6f23947864b57abc144584f44bf32befa69
vemolista <tomas@tomasvemola.com>
2026-05-15 14:48:42 +0200

    Discard stderr from hegel-core
    Some messages from hegel-core were leaking to stdout since Erlang ports just bundle stdout and stderr together.

--- src/hegel_ffi.erl
+++ src/hegel_ffi.erl
@@ -1,7 +1,7 @@
 %% HERE BE DRAGONS
 
 %% This FFI module was AI provided and accepted without scrutiny.
-%% If something seems wrong, it probably is - no thought was given
+%% If something seems wrong, it probably is - little thought was given
 %% to the correctness or simplicity of this code.
 
 -module(hegel_ffi).
@@ -9,14 +9,30 @@
 
 %% --- PORTS ---
 
+%% Quote a string for safe use in a POSIX shell command.
+%% Wraps in single quotes and escapes any embedded single quotes.
+shell_quote(Str) ->
+    Quoted = string:replace(Str, "'", "'\\''", all),
+    "'" ++ Quoted ++ "'".
+
 open_port(Command, Args) ->
     CommandChars = binary_to_list(Command),
     ArgChars = [binary_to_list(A) || A <- Args],
 
-    %% https://www.erlang.org/doc/apps/erts/erlang.html#open_port/2
+    %% We launch the server via /bin/sh so we can redirect its stderr
+    %% to /dev/null.  Without this, benign shutdown messages from
+    %% hegel-core's reader thread ("Reader loop exiting:
+    %% ConnectionClosedError: Connection closed") leak to the user's
+    %% terminal.
+    %%
+    %% Erlang ports do not support differentiating between stdout/stderr
+    %% messages, so wrapper script is the way to go.
+    EscapedArgs = [shell_quote(Arg) || Arg <- [CommandChars | ArgChars]],
+    ShellCmd = string:join(EscapedArgs, " ") ++ " 2>/dev/null",
+
     erlang:open_port(
-        {spawn_executable, CommandChars},
-        [{args, ArgChars}, binary, use_stdio, exit_status]
+        {spawn_executable, "/bin/sh"},
+        [{args, ["-c", ShellCmd]}, binary, use_stdio, exit_status]
     ).
 
 port_send(Port, Data) ->
